293f709775a9c8ccb95edb82b75210a05442c4c8
Satya0701 <m.satya4147@gmail.com>
2022-09-26 22:51:43 +0530

    Event Binding

---
+++ RJSNIT/DataBinding/src/components/EventsBinding.js
@@ -0,0 +1,68 @@
+import {React} from "react"
+export function EventBinding(){
+    return(<>
+    <div>
+        <ol>
+            <li>Mouse Events
+                <ul>
+                    <li>onMouseOver</li>
+                    <li>onMouseOut</li>
+                    <li>onMouseDown</li>
+                    <li>onMouseUp</li>
+                    <li>onMouseMove</li>
+                </ul>
+            </li>
+            <li>KeyBoard Events
+                <ul>
+                    <li>onKeyUp</li>
+                    <li>onKeyPress</li>
+                    <li>onKeyDown</li>
+                </ul>
+            </li>
+            <li>Button Events
+                <ul>
+                    <li>onClick</li>
+                    <li>onDbClick</li>
+                    <li>onContextMenu</li>
+                </ul>
+            </li>
+            <li>Element State Events
+                <ul>
+                    <li>onChange</li>
+                    <li>onBlur</li>
+                    <li>onFocus</li>
+                </ul>
+            </li>
+            <li>Cliboard Events
+                <ul>
+                    <li>onCut</li>
+                    <li>onCopy</li>
+                    <li>onPaste</li>
+                </ul>
+            </li>
+            <li>Timer Events
+                <ul>
+                    <li>setInterVal()</li>
+                    <li>setTimeOut()</li>
+                    <li>clearInterval()</li>
+                    <li>clearTimeOut()</li>
+                </ul>
+            </li>
+            <li>Form Events
+                <ul>
+                    <li>onSubmit</li>
+                    <li>onReset</li>
+                </ul>
+            </li>
+            <li>Touch Events
+                <ul>
+                    <li>onTouchStart</li>
+                    <li>onTouchEnd</li>
+                    <li>onTouchMove</li>
+                </ul>
+            </li>
+        </ol>
+        
+    </div>
+    </>)
+}
