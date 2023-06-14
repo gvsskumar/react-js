fc7b09d687aaad38d3a3a9d4ac363e9cb32c4d61
gvsskumar <kumar.developer0701@gmail.com>
2023-06-14 12:44:30 +0530

    Sample JSX Component

---
+++ practies/JSX/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import ExpensiveItem from './components/ExpensiveItem'
+function App() {
+  return (
+    <div>
+      <ExpensiveItem />
+    </div>
+  )
+}
+
+export default App
---
+++ practies/JSX/src/components/ExpensiveItem.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function ExpensiveItem() {
+  return (
+    <div>
+       <div>Date:14-06-23</div> 
+       <div>Car Insurance</div>
+       <div>$ 27.33</div>
+    </div>
+  )
+}
+
+export default ExpensiveItem;
